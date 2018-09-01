7db1bac835f1afa49a24950c839aab303ed05a59
Joshua Treudler <joshua@treudler.net>
2018-09-01 17:00:07 +0200

    added info

--- index.php
+++ index.php
@@ -3,3 +3,5 @@ $arrX = array("Phil zu Josh", "Phil zu Josh","Phil zu Josh", "Phil zu Josh");
 $randIndex = array_rand($arrX);
 echo $arrX[$randIndex];
 ?>
+<br><br><br>
+<a href="https://github.com/Joshua2504/wer-kommt-wohin">Fork me on GitHub</a>
